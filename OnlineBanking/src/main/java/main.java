991562239d20eea66ab1321007f9ef0bbf3c08af
Wei Tang <donatshanghai@gmail.com>
2017-06-27 11:42:48 -0400

    adds DB files
    adds DB files
    -Wei

--- OnlineBanking/src/main/java/main.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by danieltang on 6/27/17.
- */
-public class main {
-}
--- OnlineBanking/src/test/java/com/bank/model/test/UserTest.java
+++
@@ -1,7 +0,0 @@
-package com.bank.model.test;
-
-/**
- * Created by danieltang on 6/27/17.
- */
-public class UserTest {
-}
